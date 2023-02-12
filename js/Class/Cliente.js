eabceb053b9a00558f713b02713a3b6b9ea3de1e
Alonso Bonilla <luis.labp06@gmail.com>
2023-02-12 18:54:44 -0500

    I added the function delete and update client

--- js/Class/Cliente.js
+++ js/Class/Cliente.js
@@ -1,4 +1,6 @@
-export class Cliente {
+import { formulario } from "../Utilities/selectors.js";
+
+class Cliente {
     constructor(id,nombre,email,telefono,empresa) {
         this.id = id;
         this.nombre = nombre;
@@ -6,7 +8,7 @@ export class Cliente {
         this.telefono = telefono;
         this.empresa = empresa;
     }
-    agregarCliente(db, ui) {
+    agregarCliente(db,ui) {
         const transaction = db.transaction(['crm'],'readwrite');
         const objectStore = transaction.objectStore('crm');
         //Agregamos
@@ -16,9 +18,45 @@ export class Cliente {
         }
         transaction.oncomplete = function() {
             ui.imprimirAlerta('Cliente agregado correctamente');
+            //Reseteamos el formulario
+            formulario.reset();
             setTimeout(() => {
                 window.location.href = 'index.html';
             }, 3000);
         }
     }
+    
+    actualizarCliente(db,ui) {
+        const transaction = db.transaction(['crm'],'readwrite');
+        const objectStore = transaction.objectStore('crm');
+        console.log('Entramos aca!!')
+        //Editamos los datos
+        objectStore.put(this);
+        console.log(this);
+        transaction.oncomplete = function() {
+            ui.imprimirAlerta('Cliente editado correctamente');
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 2000);
+        }
+        transaction.onerror = function(e) {
+            console.log('Hubo un error' + e.target.result);
+            console.log(e.target);
+        }
+    }
+
+    eliminarCliente(db,e) {
+        const transaction = db.transaction(['crm'],'readwrite');
+        const objectStore = transaction.objectStore('crm');
+        objectStore.delete(this.id);
+
+        transaction.onerror = function() {
+            console.log('Hubo un error');
+        }
+        transaction.oncomplete = function() {
+            console.log('Eliminado correctamente');
+            e.target.parentElement.parentElement.remove();
+        }
+    }
 }
+export { Cliente };
