da4eae0d58e7ca083214183ce4c9dc3d3d5feaf4
Alonso Bonilla <luis.labp06@gmail.com>
2023-02-11 19:42:45 -0500

    Selectors

---
+++ js/Utilities/selectors.js
@@ -0,0 +1,7 @@
+//Campos del formulario
+export const nombreI = document.querySelector('#nombre');
+export const emailI = document.querySelector('#email');
+export const telefonoI = document.querySelector('#telefono');
+export const empresaI = document.querySelector('#empresa');
+
+export const formulario = document.querySelector('#formulario');
