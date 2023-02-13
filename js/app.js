80be95c717fadc5b2e32b15af17f5930d9463c11
Alonso Bonilla <luis.labp06@gmail.com>
2023-02-12 19:12:30 -0500

    Fixed the error of exporting from the parent module

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
 import { iniciarlizarDB} from "./indexedDB.js";
 import { UI } from './Class/UI.js';
 
-export const ui = new UI();
+const ui = new UI();
 document.addEventListener('DOMContentLoaded', () => {
     //Inicializando la BD
     iniciarlizarDB(db => {
         ui.imprimirClientes(db);
     });
 })
-
-
