5fdc6805da734570d777777003454632dbc469d7
Alonso Bonilla <luis.labp06@gmail.com>
2023-02-12 19:14:37 -0500

    I added the functionalities for delete and update

--- js/Utilities/functions.js
+++ js/Utilities/functions.js
@@ -1,27 +1,65 @@
 import { Cliente } from "../Class/Cliente.js";
-import { nombreI, emailI, telefonoI, empresaI, formulario } from './selectors.js';
+import { UI } from "../Class/UI.js";
+import { nombreI, emailI, telefonoI, empresaI } from './selectors.js';
 import { db } from "../indexedDB.js";
-import { ui } from "../app.js";
 
-export function validarCliente(e) {
+let editando = false;
+function validarCliente(e) {
     e.preventDefault();
+    const ui = new UI();
+
     const nombre = nombreI.value;
     const email = emailI.value;
     const telefono = telefonoI.value;
     const empresa = empresaI.value;
-
+    
     if(nombre === '' || email === '' || telefono === '' || empresa === '') {
         ui.imprimirAlerta('Todos los campos son obligatorios', 'error');
         return;
     }
-    //Instanciamos un cliente
-    const cliente = new Cliente(Date.now(),nombre,email,telefono,empresa);
-    //Agregamos a la bd
-    cliente.agregarCliente(db, ui);
-    //Reseteamos el formulario
-    formulario.reset();
+    if(editando) {
+        const parametrosURL = new URLSearchParams(window.location.search);
+        const idClient = Number(parametrosURL.get('id'));
+        //Intanciamos un cliente
+        const cliente = new Cliente(idClient,nombreI.value,emailI.value,telefonoI.value,empresaI.value);
+        //Actualizamos el cliente
+        cliente.actualizarCliente(db,ui);
+        editando = false;
+    } else {
+        //Instanciamos un cliente
+        const cliente = new Cliente(Date.now(),nombre,email,telefono,empresa);
+        //Agregamos a la bd
+        cliente.agregarCliente(db,ui);
+    }
 }
-export function scripting(etiqueta, clases = [], mensaje) {
+
+function actualizarCliente(e) {
+    editando = true;
+    //Validamos los datos insertados
+    validarCliente(e);
+}
+
+function obtenerCliente(db,id) {
+    const ui = new UI();
+    const transaction = db.transaction(['crm'],'readwrite');
+    const objectStore = transaction.objectStore('crm');
+    
+    const cliente = objectStore.openCursor();
+    cliente.onsuccess = function(e) {
+        const cursor = e.target.result;
+        if(cursor) {
+            if(cursor.value.id === id) {
+                ui.llenarFormulario(cursor.value);
+            }
+            cursor.continue();
+        }
+    }
+}
+
+function eliminarCliente(e,cliente) {
+    cliente.eliminarCliente(db,e);
+}
+function scripting(etiqueta, clases = [], mensaje) {
     
     const crearEtiqueta = document.createElement(etiqueta);
     if(clases.length != 0) {
@@ -34,8 +72,10 @@ export function scripting(etiqueta, clases = [], mensaje) {
     return crearEtiqueta;
 }
 
-export function addChildNodes(node,childs) {
+function addChildNodes(node,childs) {
     childs.forEach(child => {
         node.appendChild(child);
     })
 }
+
+export {validarCliente, actualizarCliente, obtenerCliente, eliminarCliente, scripting, addChildNodes};
